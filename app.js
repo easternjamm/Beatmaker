57a46be89348a0a65f8d4d4baea6bbf0a18b3f1c
EasternJamm <apetremarianioan@gmail.com>
2023-04-12 15:41:08 +0300

    4. Looping thought pads and playing sounds

--- app.js
+++ app.js
@@ -6,16 +6,38 @@ class DrumKit {
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
-    this.btm = 150;
+    this.bpm = 150;
   }
+  // Toggle .active class on pads
   activePad() {
     this.classList.toggle("active");
   }
+  // Increase the index while looping over the pads
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
+    // Loop over the pads and animate them
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      // Check if pads are active and play the respective sound if they are
+      if (bar.classList.contains("active")) {
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
+  // Starts the playback and sets speed in function of bpm
   start() {
     const interval = (60 / this.bpm) * 1000;
     setInterval(() => {
@@ -26,10 +48,14 @@ class DrumKit {
 
 const drumKit = new DrumKit();
 
+// Add a click event on all pads
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
-
+// Add a click event on the play button
 drumKit.playBtn.addEventListener("click", function () {
   drumKit.start();
 });
